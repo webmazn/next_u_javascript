99019ff4e1cc52d699105809f8d91a5b5c1ba7ba
Maycol Zambrano Nuñez <maycol_630@hotmail.com>
2019-01-24 23:11:15 -0500

    Todas las operaciones básicas

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var calculadora = ( function(){
   var nuevoFormato='';
   var nuevoValor='';
   var simbolosConsiderados = ["+", "-", "*", "/", '', ' '];
+  var operacionMatriz = new Array();
 
   var limpiarDisplay = function(){
     valoresConcatenados='';
@@ -13,48 +14,89 @@ var calculadora = ( function(){
     display.innerHTML = '0';
   }
 
+  var mostrarResultado = function(resultado){
+    display.innerHTML = resultado;
+  }
+
   var quitarValor = function(){
     nuevoValor = nuevoValor.substring(0, nuevoValor.length -1);
     display.innerHTML = nuevoValor;
   }
 
   var validarLongitud = function(valor){
-    let longitudPermitida = 8;
+    let longitudPermitida = 7;
     let longitudActual = valor.length;
     let respuesta = false;
     if(longitudActual <= longitudPermitida) respuesta = true;
     return respuesta;
   }
 
+  var seleccionarOperacion = function(operacion){
+    operacionMatriz[0]=nuevoValor;
+    operacionMatriz[1]=operacion;
+    limpiarDisplay();
+    console.log(operacionMatriz);
+  }
+
+  var sumarValores = function(x, y){
+    return operacionMatriz[3] = eval(x+'+'+y).toFixed();
+  }
+
+  var restarValores = function(x, y){
+    return operacionMatriz[3] = eval(x+'-'+y).toFixed();
+  }
+
+  var multiplicarValores = function(x, y){
+    return operacionMatriz[3] = eval(x+'*'+y).toFixed();
+  }
+
+  var dividirValores = function(x, y){
+    return operacionMatriz[3] = eval(x+'/'+y).toFixed();
+  }
+
+  var ejecutarOperacion = function(operacion){
+    switch(operacion){
+      case '+': mostrarResultado(sumarValores(operacionMatriz[0],operacionMatriz[2])); break;
+      case '-': mostrarResultado(restarValores(operacionMatriz[0],operacionMatriz[2])); break;
+      case '*': mostrarResultado(multiplicarValores(operacionMatriz[0],operacionMatriz[2])); break;
+      case '/': mostrarResultado(dividirValores(operacionMatriz[0],operacionMatriz[2])); break;
+    }
+  }
+
   var agregarValor = function(valor) {
-    let caracteres = display.textContent;//.split('');
-    let longitud = caracteres.length;
+    let caracteres = display.textContent;
+    let longitud = nuevoValor.length;
     let ultimo = '';
     let penultimo = '';
-    //localStorage.valorOperacion = valor
-    if(valor==0 && longitud==1){//|| valor=='.'
+
+    if(valor==0 && longitud==0){
       limpiarDisplay();
     }else{
       valoresConcatenados += String(valor);
       ultimo = valoresConcatenados.substring(valoresConcatenados.length -1 , valoresConcatenados.length);
       penultimo = valoresConcatenados.substring(valoresConcatenados.length -2 , valoresConcatenados.length -1);
 
-      if(simbolosConsiderados.indexOf(penultimo) != -1 && ultimo=='.'){//!/^\d+$/.test(penultimo) > si no es un número
+      console.log('ultimo '+ultimo+' / penultimo '+penultimo);
+
+      if(simbolosConsiderados.indexOf(penultimo) != -1 && ultimo=='.'){
         nuevoFormato = '0.';
       }else if(penultimo=='.' && !/^\d+$/.test(ultimo)){//}else if(penultimo=='.' && !/^\d+$/.test(ultimo)){
-        nuevoValor = nuevoValor.substring(0, nuevoValor.length -1);//quitamos el punto
-        nuevoFormato = String(valor);
-      }else if(simbolosConsiderados.indexOf(penultimo) != -1 && simbolosConsiderados.indexOf(ultimo) != -1){
         nuevoValor = nuevoValor.substring(0, nuevoValor.length -1);//quitamos el punto
         nuevoFormato = String(valor);
       }else{
         nuevoFormato = String(valor);
       }
-      nuevoValor += nuevoFormato;
-      if(validarLongitud(nuevoValor)) display.innerHTML = String(nuevoValor);
+
+      console.log(nuevoFormato);
+
+      if(validarLongitud(nuevoValor)){
+        nuevoValor += nuevoFormato;
+        //display.innerHTML = String(nuevoValor);
+        mostrarResultado(String(nuevoValor));
+      }
     }
     console.log('caracteres: '+caracteres+' / longitud: '+longitud+' / valor entrante: '+valor+' / nuevo valor: '+nuevoValor);
-    console.log('ultimo '+ultimo+' / penultimo '+penultimo);
+
   }
 
   return {
@@ -70,19 +112,20 @@ var calculadora = ( function(){
     capturaTecladoNumerico: function () {
       document.addEventListener("keypress", function(event){
         var codigo = event.which || event.keyCode; // || event.charCode;
-
-        console.log("Presiono: which: "+event.which+" / keyCode: "+event.keyCode+" / charCode: "+event.charCode+" / key: "+event.key);
+        //3console.log("Presiono: which: "+event.which+" / keyCode: "+event.keyCode+" / charCode: "+event.charCode+" / key: "+event.key);
         if(codigo === 13){
-          let caracteres = display.textContent;
-          console.log("Operar: "+caracteres);
+          //let caracteres = display.textContent; console.log("Operar: "+caracteres);
+          operacionMatriz[2]=nuevoValor;
+          console.log(operacionMatriz);
+          ejecutarOperacion(operacionMatriz[1]);
         }else{
           switch(codigo){
-            case  8:  quitarValor();    break; // backspace
-            case 42: agregarValor('*'); break;
-            case 43: agregarValor('+'); break;
-            case 45: agregarValor('-'); break;
+            case  8:  quitarValor();    break;
+            case 42: seleccionarOperacion('*'); break;
+            case 43: seleccionarOperacion('+'); break;
+            case 45: seleccionarOperacion('-'); break;
             case 46: agregarValor('.'); break;
-            case 47: agregarValor('/'); break;
+            case 47: seleccionarOperacion('/'); break;
             case 48: agregarValor('0'); break;
             case 49: agregarValor('1'); break;
             case 50: agregarValor('2'); break;
